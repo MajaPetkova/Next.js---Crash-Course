2036517933803669e6d03f69ad953cf10f610db1
Maja Petkova <majapetkova1@yahoo.com>
2023-10-24 16:58:39 +0200

    Add daisy UI

--- next-crush-course/next-app/app/components/AddToCart.tsx
+++ next-crush-course/next-app/app/components/AddToCart.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 const AddToCart = () => {
   return (
     <div>
-        <button onClick={()=> console.log("click")} className='p-3 my-5 bg-sky-500 text-white text-xl hover:bg-sky-700'>Add to card</button>
+        <button onClick={()=> console.log("click")} className='p-3 mr-5 my-5 bg-sky-500 text-white text-xl hover:bg-sky-700'>Add to card</button>
+        <button className='btn btn-accent'>Remove</button>
     </div>
   )
 }
