c419b3c9d7f8459bfb6d80359eea48641a14a4f7
Maja Petkova <majapetkova1@yahoo.com>
2023-10-28 17:57:20 +0200

    Add images in public folder and update navbar component and  css

--- next-full-app/src/components/navbar/Navbar.tsx
+++ next-full-app/src/components/navbar/Navbar.tsx
@@ -1,49 +1,63 @@
-import { Ultra } from 'next/font/google';
-import Link from 'next/link'
-import React from 'react';
+"use client"
+
+import { Ultra } from "next/font/google";
+import Link from "next/link";
+import React from "react";
 import styles from "./page.module.css";
 
 const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "Contacts",
-      url: "/contacts",
-    },
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contacts",
+    url: "/contacts",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 const Navbar = () => {
   return (
-    <div>
-        <Link href="/">Lammamias</Link>
-        <div className={styles.container}>
-         {links.map(x => (<Link key={x.id} href={x.url}>{x.title}</Link>))}
-        </div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        lammamias
+      </Link>
+      <div className={styles.links}>
+        {links.map((x) => (
+          <Link key={x.id} href={x.url} className={styles.link}>
+            {x.title}
+          </Link>
+        ))}
+        <button
+          className={styles.logout}
+          onClick={()=>console.log("logged out")}
+        >
+          Logout
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
