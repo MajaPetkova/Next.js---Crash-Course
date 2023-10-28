2d9a98017fb53560948890ed4f898807c648876b
Maja Petkova <majapetkova1@yahoo.com>
2023-10-28 18:06:37 +0200

    Update footer component and css

--- next-full-app/src/components/footer/Footer.tsx
+++ next-full-app/src/components/footer/Footer.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
-        <div className={styles.container}>
-            @2023 MPet. All rights reserved.
-        </div>
-        <div></div>
+    <div className={styles.container}>
+      <div>@2023 MPet. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15}className={styles.icon} alt="img fb" />
+        <Image src="/2.png" width={15} height={15}className={styles.icon} alt="img instagram" />  
+        <Image src="/3.png" width={15} height={15}className={styles.icon} alt='img twit'/>
+        <Image src="/4.png" width={15} height={15}className={styles.icon} alt='img youTube'/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
