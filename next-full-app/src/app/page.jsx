fadc9fe32fe898f343c3765a18c6735e35a105c5
Maja Petkova <majapetkova1@yahoo.com>
2023-10-29 18:39:14 +0100

    Update home page and add css

--- next-full-app/src/app/page.jsx
+++ next-full-app/src/app/page.jsx
@@ -1,8 +1,19 @@
 import React from "react"
 import styles from './page.module.css'
+import Image from "next/image"
+import Hero from "public/hero.png"
 
 export default function Home() {
   return (
-   <div className={styles.container}>Hello World</div>
+   <div className={styles.container} >
+    <div className={styles.item}>
+      <h1 className={styles.title}>Better design for your digital products</h1>
+      <p className={styles.description}>Turning your idea into reality. We bring together the teams from the global tech industry.</p>
+      <button className={styles.button}>See Our Works</button>
+    </div>
+    <div className={styles.item}>
+    <Image src={Hero} alt="home img" className={styles.img}/>
+    </div>
+   </div>
   )
 }
