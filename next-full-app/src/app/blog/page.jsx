d9dba489853faed710df48712015f662f6b01fbd
Maja Petkova <majapetkova1@yahoo.com>
2023-11-01 12:03:20 +0100

    Add fetching data

--- next-full-app/src/app/blog/page.jsx
+++ next-full-app/src/app/blog/page.jsx
@@ -3,40 +3,18 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {cache:"no-store"});
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/3396657/pexels-photo-3396657.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={300}
-            height={250}
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/3493730/pexels-photo-3493730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={300}
-            height={250}
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
+      {data.map(x => (<Link href="/blog/testId" className={styles.container} key={x.id}>
         <div className={styles.imgContainer}>
           <Image
             src="https://images.pexels.com/photos/2295744/pexels-photo-2295744.jpeg?auto=compress&cs=tinysrgb&w=400"
@@ -47,11 +25,11 @@ const Blog = () => {
           />
         </div>
         <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
+          <h1 className={styles.title}>{x.title}</h1>
+          <p className={styles.desc}>{x.body}</p>
         </div>
-      </Link>
-    </div>
+      </Link>))}
+         </div>
   );
 };
 
