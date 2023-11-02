d72a4681ae8aae77a99d1fdefd1e337e4999fb2c
Maja Petkova <majapetkova1@yahoo.com>
2023-11-02 11:42:31 +0100

    Add mongoose lib, connection to mongoDB, create models for db

--- next-full-app/src/app/portfolio/[category]/page.tsx
+++ next-full-app/src/app/portfolio/[category]/page.tsx
@@ -15,7 +15,7 @@ const getData = (cat) => {
 };
 const Category = ({ params }) => {
 const data= getData(params.category)
-console.log(data)
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
