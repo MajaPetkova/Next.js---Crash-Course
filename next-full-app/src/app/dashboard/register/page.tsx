e9f6ef4d1a2cef0319a666239f7da51a7a313e48
Maja Petkova <majapetkova1@yahoo.com>
2023-11-05 19:00:44 +0100

    Create register page

--- next-full-app/src/app/dashboard/register/page.tsx
+++ next-full-app/src/app/dashboard/register/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import styles from "./page.module.css";
+import Link from 'next/link';
 
 const Register = () => {
   return (
-    <div className={styles.container}>Register</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input  type='text' placeholder='username' className={styles.input} required/>
+        <input  type='email' placeholder='email' className={styles.input} required/>
+        <input  type='password' placeholder='password' className={styles.input} required/>
+        <button className={styles.btn}>Register</button>
+      </form>
+      <Link href="/dashboard/login">Login with an existing account</Link>
+    </div>
   )
 }
 
