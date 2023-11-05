f6692ea4d5e6495a16e2457674ba56bc2981077a
Maja Petkova <majapetkova1@yahoo.com>
2023-11-05 18:43:28 +0100

    Add signIn with google using useSession hook

--- next-full-app/src/app/dashboard/login/page.tsx
+++ next-full-app/src/app/dashboard/login/page.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import React from 'react'
 import styles from "./page.module.css";
+import { signIn } from 'next-auth/react';
 
 const Login = () => {
   return (
-    <div className={styles.container}>Login</div>
+    <div className={styles.container}>
+      <button onClick={()=>signIn("provider")}>Login with google</button>
+    </div>
   )
 }
 
